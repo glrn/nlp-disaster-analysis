2b921284343547d1e448e149effadfd7ad279cfc
Gal Ron <galron2000@gmail.com>
2017-07-09 16:52:29 +0300

    Expansion of twitter tiny urls

--- dataset_parser.py
+++ dataset_parser.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import csv
 import random
+import requests
 
 """
 This module parses CrowdFlower's dataset.
@@ -8,10 +9,22 @@ This module parses CrowdFlower's dataset.
 """
 
 DATASET_PATH = 'dataset/socialmedia-disaster-tweets-DFE.csv'
+EXPAND_TINYURL_TIMEOUT = 2.0
 
 class Relevancy:
     DISASTER = 1
     NOT_DISASTER = 2
+
+def tinyurl_to_url(tco_url, _timeout = EXPAND_TINYURL_TIMEOUT):
+    """
+    Twitter uses https://t.co/ to minify URLs in tweets.
+    Use this function to get the real URL.
+    Default timeout if 0.5s.
+    """
+    try:
+        return requests.get(tco_url, timeout=_timeout).url
+    except requests.exceptions.Timeout:
+        return None
 
 def dataset_as_dict():
     """
@@ -58,3 +71,9 @@ if __name__=='__main__':
     print '=== for example:'
     for i in random.sample(range(0, len(some_irrelevant_tweets)), 5):
         print '\t%s' % some_irrelevant_tweets[i]['text']
+
+    print
+    print '=== Another example - expand tiny urls (timeout is %dsec):' % EXPAND_TINYURL_TIMEOUT
+    urls = ['http://t.co/VpGu8z1Lhb', 'http://t.co/3sNyOOhseq', 'http://t.co/Byj5Dfa2rv']
+    for url in urls:
+        print('\t%s --> %s') % (url, tinyurl_to_url(url))
