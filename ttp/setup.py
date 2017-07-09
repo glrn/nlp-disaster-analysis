36ec443d9552f8622d485c6a02cf174c351ffb9d
Gal Ron <galron2000@gmail.com>
2017-07-09 18:04:03 +0300

    Added twitter-text-python package
    https://github.com/edburnett/twitter-text-python

---
+++ ttp/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name='twitter-text-python',
+    version='1.1.0',
+    description='Twitter Tweet parser and formatter',
+    long_description="Extract @users, #hashtags and URLs (and unwind shortened links) from tweets including entity locations, also generate HTML for output. Visit https://github.com/edburnett/twitter-text-python for examples.",
+    author='Maintained by Edmond Burnett (previously Ian Ozsvald; originally Ivo Wetzel)',
+    author_email='_@edmondburnett.com',
+    url='https://github.com/edburnett/twitter-text-python',
+    license='MIT',
+    packages=['ttp'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[],
+    classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Text Processing :: Linguistic',
+    ]
+)
---
+++ ttp/utils.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Unwind short-links e.g. bit.ly, t.co etc to their canonical links"""
+from __future__ import unicode_literals, print_function
+import requests
+
+DEFAULT_GET_TIMEOUT = 2.0
+
+def follow_shortlink(shortlink):
+    """Follow redirects of shortlink, return dict of resulting URLs"""
+    url = shortlink
+    request_result = requests.get(url, timeout = DEFAULT_GET_TIMEOUT)
+    redirect_history = request_result.history
+    # history might look like:
+    # (<Response [301]>, <Response [301]>)
+    # where each response object has a URL
+    all_urls = []
+    for redirect in redirect_history:
+        all_urls.append(redirect.url)
+    # append the final URL that we finish with
+    all_urls.append(request_result.url)
+    return all_urls
+
+
+if __name__ == "__main__":
+    shortlinks = ['http://t.co/8o0z9BbEMu', 'http://bbc.in/16dClPF']
+    print (follow_shortlinks(shortlinks))
