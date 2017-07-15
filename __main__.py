bb7bc883c4b7385e150c9fa087a497ef5dd6b9e9
Daniel <danielsolomon94.ds@gmail.com>
2017-07-15 18:17:01 +0300

    Added simple main that currenly loads the dataset and simply use the only classifier it has (bag of words) maintaining acc around 80%.

---
+++ __main__.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+import common
+
+from sklearn.cross_validation   import train_test_split
+
+from classifier                 import BagOfWords
+from dataset_parser             import dataset_as_dict, get_corpus, get_labels
+
+TEST_SLICE = 0.1
+
+def main():
+    print('Starting...')
+    print('Parsing dataset...')
+    dataset = dataset_as_dict()
+    print('Done parsing, dataset length: {}'.format(len(dataset)))
+
+    print('Splitting into train {} and test {}'.format(1 - TEST_SLICE, TEST_SLICE))
+    train, test = train_test_split(dataset, test_size=TEST_SLICE)
+
+    print('Generating corpuses and labels...')
+    train_corpus, test_corpus = get_corpus(train), get_corpus(test)
+    train_labels, test_labels = get_labels(train), get_labels(test)
+
+    print('Generating bag of words...')
+    bag = BagOfWords(train_corpus, train_labels)
+
+    print('Fitting...')
+    bag.fit_forest(n_estimators=100)
+
+    print('Predicting...')
+    result = bag.predict_forest(test_corpus)
+
+    acc = common.compute_accuracy(result, test_labels)
+    print('acc: {}', acc)
+
+if __name__ == '__main__':
+    main()
