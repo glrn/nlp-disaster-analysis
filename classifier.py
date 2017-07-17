9a07931535cf8772ba89faa686bde40533be40f3
Daniel <danielsolomon94.ds@gmail.com>
2017-07-17 20:56:45 +0300

    Added measurments to fitting functions and kwds supporting.

--- classifier.py
+++ classifier.py
@@ -1,3 +1,5 @@
+import common
+
 from sklearn.ensemble                   import RandomForestClassifier
 from sklearn.feature_extraction.text    import CountVectorizer
 from sklearn.naive_bayes                import BernoulliNB
@@ -9,21 +11,24 @@ class BagOfWords(object):
         self.bag_of_words   = self.vectorizer.fit_transform(corpus)
         self.labels         = labels
         self.vocabulary     = self.vectorizer.get_feature_names()
+        self.kwds           = kwds
 
+    @common.timeit
     def fit_forest(self, n_estimators=10):
         self.forest = RandomForestClassifier(n_estimators=n_estimators)
         self.forest.fit(self.bag_of_words, self.labels)
 
     def predict_forest(self, test):
-        vectorize   = CountVectorizer(vocabulary=self.vocabulary)
+        vectorize   = CountVectorizer(vocabulary=self.vocabulary, **self.kwds)
         bag         = vectorize.fit_transform(test)
         return self.forest.predict(bag)
 
+    @common.timeit
     def fit_naive_bayes(self):
         self.nb = BernoulliNB()
         self.nb.fit(self.bag_of_words, self.labels)
 
     def predict_naive_bayes(self, test):
-        vectorize   = CountVectorizer(vocabulary=self.vocabulary)
+        vectorize   = CountVectorizer(vocabulary=self.vocabulary, **self.kwds)
         bag         = vectorize.fit_transform(test)
         return self.nb.predict(bag)
