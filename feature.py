2702da9b92e13b4ed825b17d9ded7fb0d5e5181c
Daniel <danielsolomon94.ds@gmail.com>
2017-08-23 23:23:02 +0300

    Added feature decorator to easily add extracting new features.

---
+++ feature.py
@@ -0,0 +1,29 @@
+import collections
+import numpy as np
+
+named_features = collections.defaultdict(list)
+
+def feature(name):
+    '''
+        decorator for specific classifier, the wrapped function should accept as an input a corpus and return a matrix of vector features.
+    '''
+    def feature_wrapper(func):
+        def wrapper(*args, **kwds):
+            return func(*args, **kwds)
+        named_features[name].append(wrapper)
+        return wrapper
+    return feature_wrapper
+
+def fitter(name, inputs):
+    '''
+        The fitter function takes all features function that `name` owns and run them one by one on the inputs (corpus).
+        Each of those functions return a matrix, which they are all concatenated into one big matrix (concatenated by extending lines).
+    '''
+    matrices = []
+    for f in named_features[name]:
+        matrices.append(f(inputs))
+    a = matrices[0]
+    for b in matrices[1:]:
+        a = np.concatenate((a, b), axis=1)
+    return a
+
