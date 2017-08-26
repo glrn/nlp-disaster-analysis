713c40f915634652d4ae4cc7bbd07aeec283a44c
Gal Ron <galron2000@gmail.com>
2017-08-26 20:12:26 +0300

    Add dataset of processed tweets
    with extracted URLs and hashtags, user references replaced with token

--- dataset/extract_processed_tweets.py
+++ dataset/extract_processed_tweets.py
@@ -1,13 +1,14 @@
 import csv
+from ttp import ttp
 
-INPUT_FILE = 'socialmedia-disaster-tweets-DFE.csv'
-OUTPUT_FILE = 'socialmedia-disaster-tweets-DFE-only-tweets.txt'
+INPUT_FILE = 'socialmedia-disaster-tweets-DFE-extended.csv'
+OUTPUT_FILE = 'socialmedia-disaster-tweets-DFE-only-tweets-processed.txt'
 
 with open(INPUT_FILE, 'rb') as csvfile:
     with open(OUTPUT_FILE, 'wb') as txtfile:
         for row in csv.DictReader(csvfile):
-            tweet = row['text'].decode('utf-8').encode('ascii', 'replace').strip()
-            tweet = tweet.replace('\r','')
+            tweet = ttp.process_tweet(row)
+            tweet = tweet.replace('\r', '')
             tweet = tweet.replace('\n', '')
             txtfile.write(tweet)
             txtfile.write('\r\n')
