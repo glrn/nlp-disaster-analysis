2aaa0fcc37a6f0d7bf3f45ed619e499f9b44a863
Gal Ron <galron2000@gmail.com>
2017-09-02 21:10:50 +0300

    New library for fetching tweets using Twitter API
    Note: You should supply your own credentials

---
+++ twitter_api/__init__.py
@@ -0,0 +1,4 @@
+
+if __name__ == '__main__':
+    import StreamingAPI
+    StreamingAPI.print_tweet_stream()
---
+++ twitter_api/_credentials.py
@@ -0,0 +1,6 @@
+# Fill here your own Twitter Application Credentials (see https://apps.twitter.com/)
+
+ACCESS_TOKEN =      ''
+ACCESS_SECRET =     ''
+CONSUMER_KEY =      ''
+CONSUMER_SECRET =   ''
