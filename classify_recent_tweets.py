60bec2207162f8a829e4cb3c1dda038b106752b9
Gal Ron <galron2000@gmail.com>
2017-09-03 16:15:16 +0300

    Results of SVM classification on Chicago & Houston tweets

--- classify_recent_tweets.py
+++ classify_recent_tweets.py
@@ -2,6 +2,7 @@ from sklearn import svm
 from sklearn.cross_validation import train_test_split
 import csv
 import classifier
+import collections
 import common
 import dataset_parser.tweet_parser
 import numpy
@@ -12,11 +13,16 @@ from shutil import copyfile
 TEST_SLICE = 0.1
 
 def main():
-    TWEET_CSV_PATH = "dataset/recent_tweets_test/400_chicago_tweets.csv"
-    EXTENDED_CSV_PATH = "dataset/recent_tweets_test/400_chicago_tweets-extended.csv"
-    POS_TAG_PATH = "dataset/recent_tweets_test/400_chicago_tweets-POS-Tagging.txt"
-    NER_TAG_PATH = "dataset/recent_tweets_test/400_chicago_tweets-NER-tags.txt"
-    OUT_CSV_PATH = "dataset/recent_tweets_test/400_chicago_tweets-labeled.csv"
+    TWEET_CSV_PATH = "dataset/recent_tweets_test/chicago_tweets.csv"
+    EXTENDED_CSV_PATH = "dataset/recent_tweets_test/chicago_tweets-extended.csv"
+    POS_TAG_PATH = "dataset/recent_tweets_test/chicago_tweets-POS-Tagging.txt"
+    NER_TAG_PATH = "dataset/recent_tweets_test/chicago_tweets-NER-tags.txt"
+    OUT_CSV_PATH = "dataset/recent_tweets_test/chicago_tweets-labeled.csv"
+    # TWEET_CSV_PATH = "dataset/recent_tweets_test/houston_tweets.csv"
+    # EXTENDED_CSV_PATH = "dataset/recent_tweets_test/houston_tweets-extended.csv"
+    # POS_TAG_PATH = "dataset/recent_tweets_test/houston_tweets-POS-Tagging.txt"
+    # NER_TAG_PATH = "dataset/recent_tweets_test/houston_tweets-NER-tags.txt"
+    # OUT_CSV_PATH = "dataset/recent_tweets_test/houston_tweets-labeled.csv"
 
     labeled_dataset = Dataset()
     unlabeled_dataset = Dataset(dataset_path = EXTENDED_CSV_PATH,
@@ -42,15 +48,23 @@ def main():
     fieldnames = ['timestamp','location','text','choose_one','choose_one:confidence']
     writer = csv.DictWriter(open(OUT_CSV_PATH, 'wb'), fieldnames)
     writer.writeheader()
+
+    disaster_named_entities = []
     i = 0
     for line in lines:
         if labels[i] == 0:
             line["choose_one"] = "Not Relevant"
         else:
             line["choose_one"] = "Relevant"
+            disaster_named_entities.extend(unlabeled_dataset.entries[i].named_entities)
         writer.writerow(line)
+        print '[%s] %s' % (line["choose_one"], line["text"])
         i += 1
 
+    print '=== 20 most frequent disaster-related named-entities found ==='
+    counter = collections.Counter(disaster_named_entities)
+    for ne in sorted(counter, key=counter.get, reverse=True)[:20]:
+        print '%s : %d' % (ne,counter[ne])
 
 
 if __name__ == '__main__':
